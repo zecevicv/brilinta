c9b5eff2f9c7c26a32c430aeb1895853ea54fb6a
Vladimir Zecevic <zecevicvladimir1@gmail.com>
2022-01-25 15:44:43 +0100

    [ADD] - Post Components

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+/* #Global
+  ======================================================= */
+const body = document.querySelector('body')
+
 /* #Category Sliders
   ======================================================= */
 // Top Slider
@@ -37,5 +41,76 @@ if (document.querySelector('.category .bottom .swiper')) {
         loop: false
       }
     }
+  });
+}
+
+/* #Post Cards Slider
+  ======================================================= */
+if (document.querySelector('.post-cards .swiper')) {
+  new Swiper(".post-cards .swiper", {
+    pagination: {
+      el: ".post-cards .swiper-pagination",
+    },
+    breakpoints: {
+      0: {
+        allowTouchMove: true,
+        centeredSlides: true,
+        slidesPerView: 1.35,
+        loop: true
+      },
+      1024: {
+        allowTouchMove: false,
+        loop: false,
+        slidesPerView: 3
+      }
+    }
+  });
+}
+
+/* #Popup
+  ======================================================= */
+const popups = document.querySelectorAll('.popup');
+const popupTogglers = document.querySelectorAll('.popup-toggler');
+const popupClosers = document.querySelectorAll('.popup-close');
+const popupBackdrops = document.querySelectorAll('.popup-backdrop');
+
+if (popups && popupTogglers) {
+  popups.forEach((popup) => {
+    gsap.set(popup, {
+      display: 'flex'
+    });
+
+    popup.addEventListener('click', (e) => {
+      if (!e.target.closest('.popup-content')) {
+        body.classList.remove('no-scroll');
+        popup.classList.remove('show');
+      }
+    });
+  });
+
+  popupTogglers.forEach((toggler) => {
+
+    toggler.addEventListener('click', (e) => {
+      e.preventDefault();
+
+      const popup = document.querySelector(toggler.dataset.popup);
+
+      body.classList.add('no-scroll');
+      popup.classList.add('show');
+    });
+  });
+}
+
+if (popupClosers) {
+  popupClosers.forEach((closer) => {
+    closer.addEventListener('click', (e) => {
+      e.preventDefault();
+
+      const popup = closer.closest('.popup');
+
+      body.classList.remove('no-scroll');
+      popup.classList.remove('show');
+
+    })
   });
 }
